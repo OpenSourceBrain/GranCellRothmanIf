ebb3b25e282621432ca1112cb832579d8bcd2a9c
Eugenio Piasini <e.piasini@ucl.ac.uk>
2013-05-01 14:16:40 +0100

    Add comparison between Rothman2009 and Schwartz2012 LEMS block model.

--- neuroConstruct/lemsSimulations/voltageSweep/block_factor_plot.py
+++ neuroConstruct/lemsSimulations/voltageSweep/block_factor_plot.py
@@ -5,13 +5,16 @@ from matplotlib import pyplot as plt
 
 lems_data = np.loadtxt('blockFactorVSVoltage.dat')
 voltage = lems_data[:,1] * 1000. # (mV)
-block_factor = lems_data[:,2]
+block_factor_Rothman = lems_data[:,2]
+block_factor_Ward = lems_data[:,3]
 
 fig, ax = plt.subplots()
-ax.fill_between(voltage, block_factor, color="g")
+ax.fill_between(voltage, block_factor_Rothman, color="g")
+ax.fill_between(voltage, block_factor_Ward, color="r", alpha=0.5)
 ax.set_xlabel('membrane voltage (mV)')
 ax.set_ylabel('gNMDA unblocked fraction')
 ax.set_xlim(left=-102, right=42)
 ax.set_ylim(bottom=0, top=1)
 ax.grid()
+
 plt.show()
